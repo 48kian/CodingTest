f6c63680d25f0c5ddb3e04c5bfcf8816e41e1824
48kian <48kian@gmail.com>
2020-06-03 14:38:15 +0900

    targetNumber

--- 프로그래머스/DFS,BFS/targetNumber.py
+++ 프로그래머스/DFS,BFS/targetNumber.py
@@ -1,1 +1,32 @@
-print(1)
+numbers = [1, 1, 1, 1, 1]
+target = 3
+
+def solution(numbers, target):
+
+    
+    index = 0
+    count = 0
+
+
+    def dfs(numbers, target, index):
+
+        if(index < len(numbers)) :
+            dfs(numbers, target, index + 1)
+            numbers[index] = numbers[index] * (-1)
+            dfs(numbers, target, index + 1)
+
+        elif(index == len(numbers)) :
+            if(sum(numbers) == target) :
+               nonlocal count
+               count += 1
+
+        return 
+
+    dfs(numbers, target, index)
+
+    return count
+
+
+answer = solution(numbers, target)
+
+print(answer)
